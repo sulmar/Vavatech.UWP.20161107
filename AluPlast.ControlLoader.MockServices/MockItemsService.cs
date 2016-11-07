64ba330930a990ca806714aa34ec8cf907fbffee
Marcin Sulecki <marcin.sulecki@gmail.com>
2016-11-07 11:45:51 +0100

    Dodanie mocków

---
+++ AluPlast.ControlLoader.MockServices/MockItemsService.cs
@@ -0,0 +1,32 @@
+﻿using AluPlast.ControlLoader.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AluPlast.ControlLoader.Models;
+
+namespace AluPlast.ControlLoader.MockServices
+{
+    public class MockItemsService : IItemsService
+    {
+        private IList<Item> _Items = new List<Item>
+        {
+            new Item { ItemId = 1, ItemType = ItemType.EUR, IsLoaded = false },
+            new Item { ItemId = 2, ItemType = ItemType.PAL, IsLoaded = false },
+            new Item { ItemId = 3, ItemType = ItemType.SZY, IsLoaded = true },
+            new Item { ItemId = 4, ItemType = ItemType.EUR, IsLoaded = false },
+        };
+
+
+        public IList<Item> Get(int loadId)
+        {
+            return _Items;
+        }
+
+        public void Update(Item item)
+        {
+            item.IsLoaded = true;
+        }
+    }
+}
---
+++ AluPlast.ControlLoader.MockServices/MockPhotosService.cs
@@ -0,0 +1,30 @@
+﻿using AluPlast.ControlLoader.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AluPlast.ControlLoader.Models;
+
+namespace AluPlast.ControlLoader.MockServices
+{
+    public class MockPhotosService : IPhotosService
+    {
+        private IList<Photo> _Photos;
+
+        public void Add(Photo photo)
+        {
+            _Photos.Add(photo);
+        }
+
+        public IList<Photo> Get(int loadId)
+        {
+            return _Photos;
+        }
+
+        public void Remove(int photoId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
