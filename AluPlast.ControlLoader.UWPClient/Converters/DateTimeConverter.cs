7e28ee5b1987e3fe45e823717708ee1f4ea77f57
Marcin Sulecki <marcin.sulecki@gmail.com>
2016-11-10 09:16:14 +0100

    Dodanie wyboru daty

---
+++ AluPlast.ControlLoader.UWPClient/Converters/DateTimeConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace AluPlast.ControlLoader.UWPClient.Converters
+{
+    public class DateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var date = (DateTime)value;
+
+            return new DateTimeOffset(date);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            var dateoffset = (DateTimeOffset)value;
+
+            return dateoffset.Date;
+        }
+    }
+}
