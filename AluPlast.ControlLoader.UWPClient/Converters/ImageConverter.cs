6ab6ee7f441de58ab690a3341c30a56bd09db534
Marcin Sulecki <marcin.sulecki@gmail.com>
2016-11-12 17:06:05 +0100

    Dodanie konwertera byte[] na image

--- AluPlast.ControlLoader.UWPClient/Converters/ImageConverter.cs
+++ AluPlast.ControlLoader.UWPClient/Converters/ImageConverter.cs
@@ -2,30 +2,49 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Graphics.Imaging;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Media.Imaging;
 
 namespace AluPlast.ControlLoader.UWPClient.Converters
 {
+
+    /// <summary>
+    /// na podst. https://marcominerva.wordpress.com/2013/04/15/how-to-convert-a-byte-array-to-image-in-a-windows-store-app/
+    /// </summary>
     public class ImageConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            var bytes = (byte[])value;
 
-            using (MemoryStream stream = new MemoryStream(bytes))
+            if (value == null || !(value is byte[]))
+                return null;
+
+            var buffer = (byte[])value;
+
+            using (InMemoryRandomAccessStream ms = new InMemoryRandomAccessStream())
             {
-                var bitmapImage = new BitmapImage();
-                stream.Seek(0, SeekOrigin.Begin);
-                bitmapImage.SetSource(stream.AsRandomAccessStream());
+                // Writes the image byte array in an InMemoryRandomAccessStream
+                // that is needed to set the source of BitmapImage.
+                using (DataWriter writer = new DataWriter(ms.GetOutputStreamAt(0)))
+                {
+                    writer.WriteBytes(buffer);
 
-                return bitmapImage;
+                    // The GetResults here forces to wait until the operation completes
+                    // (i.e., it is executed synchronously), so this call can block the UI.
+                    writer.StoreAsync().GetResults();
+                }
+
+                var image = new BitmapImage();
+                image.SetSource(ms);
+                return image;
             }
-            
+
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
